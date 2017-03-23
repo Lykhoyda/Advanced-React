692e6ccea884e4b48df18157c1ab648690a0a562
MacMini <MacMini@mac-mini.local>
2017-03-23 17:05:31 +0200

    add hight order component

---
+++ higher-order-component/src/components/resources.js
@@ -0,0 +1,14 @@
+import React, { component } from 'react';
+
+export default () => {
+    return (
+        <div>
+            Super Special Secret Recipe
+            <ul>
+                <li>B</li>
+                <li>M</li>
+                <li>W</li>
+            </ul>    
+        </div>
+    )
+}
