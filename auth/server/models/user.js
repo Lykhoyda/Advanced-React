54c3865196e1a5760795f87a97fea3cc754f13cd
MacMini <MacMini@mac-mini.local>
2017-03-29 21:12:38 +0300

    client auth add

--- auth/server/models/user.js
+++ auth/server/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt-nodejs')
 const Schema = mongoose.Schema;
 
 // Define model
@@ -11,6 +12,29 @@ const userSchema = new Schema({
     },
     password: String
 });
+// On save Hook
+userSchema.pre('save', function(next) {
+    const user = this;
+
+    bcrypt.genSalt(10, function(err, salt) {
+        if (err) { return next(err); }
+
+        bcrypt.hash(user.password, salt, null, function(err, hash){
+            if (err) { return next(err); }
+
+            user.password = hash;
+            next();
+        });
+    });
+});
+
+userSchema.methods.comparePassword = function (candidatePassword, callback) {
+    bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+        if (err) { return callback(err); }
+
+        callback(null, isMatch);
+    })
+}
 
 // Create the model Class
 const ModelClass = mongoose.model('user', userSchema);
